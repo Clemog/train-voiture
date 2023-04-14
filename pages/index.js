cbe4c0d6ead5e008a7771545c67c1cd0df476d28
Clement AUGER <clem.auger@hotmail.fr>
2023-04-15 00:23:22 +0200

    fix Link

--- pages/index.js
+++ pages/index.js
@@ -9,28 +9,18 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <meta
-          name='description'
-          content='Une app pour choisir entre train et voiture'
+          name="description"
+          content="Une app pour choisir entre train et voiture"
         />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src='/next.svg'
-            alt='Next.js Logo'
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-        <p>
+        <div>
           {' '}
-          Visitez la première page du site sur les{' '}
-          <Link href='trajet'>"Trajets"</Link>
-        </p>
+          Visitez la première page du site pour comparer train et voiture:
+        </div>
+        <Link href="/trajet">J'y vais !</Link>
       </main>
     </>
   )
